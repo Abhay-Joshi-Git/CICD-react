eadaa8b080a4335ce386eeb4a7195f676ff878cb
Abhay Joshi <abhy.joshi@gmail.com>
2019-02-24 14:02:45 +0530

    added initial app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './cicd-logo.jpg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span class="center">
+            <span>Simple CICD App</span>
+          </span>
+        </header>
+        <div class="App-body">
+          main content
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
