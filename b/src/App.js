d5665740e7d0aa227c146f84d621269168d09700
Abhay Joshi <abhay.joshi@synerzip.com>
2020-02-26 15:53:18 +0530

    multi-branch

---
+++ b/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './cicd-logo.jpg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="center">
+            <span>Simple react application</span>
+          </span>
+        </header>
+        <div className="App-body">
+          some main content
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
