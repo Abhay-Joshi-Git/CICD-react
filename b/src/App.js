393cc7a8704b1f48da0a528fa3d4c1c75399a3ec
Abhay Joshi <abhay.joshi@synerzip.com>
2020-02-28 19:29:27 +0530

    adding multi project - conditional deploy

--- b/src/App.js
+++ b/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <span className="center">
-            <span>Simple react application</span>
+            <span>application B</span>
           </span>
         </header>
         <div className="App-body">
